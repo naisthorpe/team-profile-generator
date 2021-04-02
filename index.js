02972e4ed73539e57349bc7012bd069e9951432d
naisthorpe <aisthorpe@gmail.com>
2021-04-02 13:35:17 -0400

    Add rest inquirer questions for engineer and intern

--- index.js
+++ index.js
@@ -2,32 +2,102 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// Create an array of questions for user input
-const questions = [
+// Manager questions
+const managerQs = [
     { 
         type: "input",
         message: "What is the team manager's name?",
-        name: "manager-name",
+        name: "name",
     },
     {
         type: "input",
         message: "What is the team manager's id?",
-        name: "manager-id",
+        name: "id",
     },
     {
         type: "input",
         message: "What is the team manager's email?",
-        name: "manager-email",
+        name: "email",
     },
     {
         type: "input",
         message: "What is the team manager's office number?",
-        name: "manager-office",
+        name: "officeNumber",
     },
     {
         type: "list",
         message: "Which type of team member would you like to add? (Use arrow keys)",
-        name: "member-add",
+        name: "add",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add anymore team members"
+        ],
+    },
+    
+];
+
+// Engineer questions
+const engineerQs = [
+    { 
+        type: "input",
+        message: "What is the engineer's name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is the engineer's id?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is the engineer's email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the engineer's GitHub username?",
+        name: "github",
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add? (Use arrow keys)",
+        name: "add",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add anymore team members"
+        ],
+    },
+    
+];
+
+// Intern questions
+const internQs = [
+    { 
+        type: "input",
+        message: "What is the intern's name?",
+        name: "name",
+    },
+    {
+        type: "input",
+        message: "What is the intern's id?",
+        name: "id",
+    },
+    {
+        type: "input",
+        message: "What is the intern's email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "school",
+    },
+    {
+        type: "list",
+        message: "Which type of team member would you like to add? (Use arrow keys)",
+        name: "add",
         choices: [
             "Engineer",
             "Intern",
