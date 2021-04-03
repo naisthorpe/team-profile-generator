711354e29dcf9b0ebf11eecc29757c283312e643
naisthorpe <aisthorpe@gmail.com>
2021-04-03 18:37:13 -0400

    Build intern class based on intern test file and fix employee function returns

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,17 +1,20 @@
 const Intern = require("../lib/Intern");
 
+// Intern should have a school parameter in constructor
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
 
+// getRole should return "Intern"
 test("getRole() should return \"Intern\"", () => {
   const testValue = "Intern";
   const e = new Intern("Foo", 1, "test@test.com", "UCLA");
   expect(e.getRole()).toBe(testValue);
 });
 
+// getSchool should return school
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
   const e = new Intern("Foo", 1, "test@test.com", testValue);
