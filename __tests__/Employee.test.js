ef61cdf8127bbdc13b8bdd0033a6e2c7d14444cc
naisthorpe <aisthorpe@gmail.com>
2021-04-03 17:58:41 -0400

    Add comments to employee test file to help with understanding

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -5,42 +5,50 @@ test("Can instantiate Employee instance", () => {
   expect(typeof(e)).toBe("object");
 });
 
+// Testing for a name variable in constructor
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
 
+// Testing for an id variable in constructor
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
 
+// Testing for an email variable in constructor
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
 
+// Testing for getName() function
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
 
+// Testing for getID() function
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
 
+// Testing for getEmail() function
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
+// Testing for getRole() function
+// Role should be Employee
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
