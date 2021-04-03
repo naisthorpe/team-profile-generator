c216be7a33ed5d5126bf7b836b0384550c33e363
naisthorpe <aisthorpe@gmail.com>
2021-04-03 18:22:59 -0400

    Build out manager class based on manager test file

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,18 +1,21 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
+// There should be an officeNumber parameter in the constructor
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
+// The getRole function should return "Manager"
 test("getRole() should return \"Manager\"", () => {
   const testValue = "Manager";
   const e = new Manager("Foo", 1, "test@test.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
 
+// There should be a getOfficeNumber function
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
   const e = new Manager("Foo", 1, "test@test.com", testValue);
