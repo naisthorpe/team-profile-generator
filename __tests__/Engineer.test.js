4d35daff23be8015f97cd543ac76b6ebe40729e7
naisthorpe <aisthorpe@gmail.com>
2021-04-03 18:29:14 -0400

    Build engineer class based on engineer test file

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,17 +1,20 @@
 const Engineer = require("../lib/Engineer");
 
+// Engineer should have github parameter in constructor
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
+// getRole function should return "Engineer"
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
   const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
   expect(e.getRole()).toBe(testValue);
 });
 
+// getGithub should return the GitHub username
 test("Can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
